04e81252257985db04ec198e8f9d3918dfd9a085
yadavhimanshu1811 <yadavhimanshu1811@gmail.com>
2025-12-03 21:15:32 +0530

    Added loader, infinite scroll, noImage placeholder

--- frontend/src/Components/GetProduct/GetProduct.tsx
+++ frontend/src/Components/GetProduct/GetProduct.tsx
@@ -3,6 +3,7 @@ import "./GetProduct.css";
 import { Link } from "react-router-dom";
 import { useNotification } from "../../context/NotificationContext";
 import Loader from "../Loader/Loader";
+import noImage from "../../assets/noImage.svg";
 
 export interface User {
   _id: string;
@@ -26,12 +27,12 @@ const GetProduct = () => {
   const [products, setProducts] = useState([] as any[]);
   const [totalProducts, setTotalProducts] = useState(0);
   const [page, setpage] = useState(1);
-  const limit = 4;
+  const limit = 5;
   const [loading, setloading] = useState(true);
   const [isSearch, setIsSearch] = useState(false);
   const currentUserId = JSON.parse(localStorage.getItem("user") ?? "{}")?._id;
 
-  const getProducts = async (afterSearch ?: Boolean) => {
+  const getProducts = async (afterSearch?: Boolean) => {
     const API = import.meta.env.VITE_API_URL;
     const response = await fetch(
       `${API}/getproducts?page=${afterSearch ? 1 : page}&limit=${limit}`,
@@ -66,7 +67,9 @@ const GetProduct = () => {
       showNotification(result.error, "error");
       setloading(false);
     } else {
-      getProducts();
+      setpage(1);
+      setProducts([]);
+      getProducts(true);
       showNotification("Product deleted successfully", "success");
     }
   };
@@ -82,48 +85,80 @@ const GetProduct = () => {
     getProducts();
   }, [page]);
 
- const debounceTimer = useRef<ReturnType<typeof setTimeout> | null>(null);
+  const debounceTimer = useRef<ReturnType<typeof setTimeout> | null>(null);
 
-const handleSearch = (searchString: string) => {
-  setIsSearch(true);
-  setloading(true);
-  if (debounceTimer.current) {
-    clearTimeout(debounceTimer.current);
-  }
-
-  debounceTimer.current = setTimeout(async () => {
-    if (searchString.trim()) {
-      const API = import.meta.env.VITE_API_URL;
-      const response = await fetch(`${API}/search/${searchString}`, {
-        headers: {
-          authorization: JSON.parse(localStorage.getItem("token") || ""),
-        },
-      });
-      const result = await response.json();
-      console.log("search result", result);
-      if (result) {
-        setProducts(result);
-      }
-      setloading(false);
-    } else {
-      // if empty → reset pagination
-      setProducts([]);
-      setpage(1);
-      setloading(true);
-      getProducts(true);
-      setIsSearch(false);
+  const handleSearch = (searchString: string) => {
+    setIsSearch(true);
+    setloading(true);
+    if (debounceTimer.current) {
+      clearTimeout(debounceTimer.current);
     }
-  }, 500); // Recommended: 300-500ms debounce
-};
 
-  const handleLoadMore = () => {
-    setpage((prev) => prev + 1);
+    debounceTimer.current = setTimeout(async () => {
+      if (searchString.trim()) {
+        const API = import.meta.env.VITE_API_URL;
+        const response = await fetch(`${API}/search/${searchString}`, {
+          headers: {
+            authorization: JSON.parse(localStorage.getItem("token") || ""),
+          },
+        });
+        const result = await response.json();
+        console.log("search result", result);
+        if (result) {
+          setProducts(result);
+        }
+        setloading(false);
+      } else {
+        // if empty → reset pagination
+        setProducts([]);
+        setpage(1);
+        setloading(true);
+        getProducts(true);
+        setIsSearch(false);
+      }
+    }, 500); // Recommended: 300-500ms debounce
   };
+
+  // const handleLoadMore = () => {
+  //   setpage((prev) => prev + 1);
+  // };
+
+  const listRef = useRef(null);
+  const loaderRef = useRef(null);
+
+  useEffect(() => {
+    if (loading) return; // wait until products are rendered
+    if (!loaderRef.current) return;
+    if (!listRef.current) return;
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          console.log("REACHED BOTTOM");
+          setpage((prev) => prev + 1);
+        }
+      },
+      {
+        root: listRef.current,
+        rootMargin: "0px",
+        threshold: 0.1,
+      }
+    );
+
+    observer.observe(loaderRef.current);
+
+    return () => observer.disconnect();
+  }, [loading, products.length]);
 
   return (
     <div className="product-container">
       <div className="product-container-div">
-        <h1>Products List {loading ? "" : `(${isSearch ? products.length : totalProducts} items)`}</h1>
+        <h1>
+          Products List{" "}
+          {loading
+            ? ""
+            : `(${isSearch ? products.length : totalProducts} items)`}
+        </h1>
         <input
           placeholder="Search product"
           onChange={(e) => handleSearch(e.target.value)}
@@ -146,7 +181,7 @@ const handleSearch = (searchString: string) => {
             <Loader />
           </div>
         ) : products.length ? (
-          <div className="product-list">
+          <div className="product-list" ref={listRef}>
             {products.map((item: Product, index: number) => {
               return (
                 <div className="row" key={item._id}>
@@ -156,7 +191,7 @@ const handleSearch = (searchString: string) => {
                   <div className="cell">{item.company}</div>
                   <div className="cell">{item.category}</div>
                   <div className="cell">{item.contact}</div>
-                  <img src={item.image} alt="product" width="80" height="80" />
+                  <img src={item.image || noImage} alt="product" height="80" />
                   {item.user._id === currentUserId ? (
                     <div className="cell">
                       <button
@@ -190,10 +225,16 @@ const handleSearch = (searchString: string) => {
                 </div>
               );
             })}
-            {products.length < totalProducts && !isSearch ? (
+            {/* {products.length < totalProducts && !isSearch ? (
               <div className="load-more">
                 <button onClick={handleLoadMore}>Load more</button>
               </div>
+            ) : null} */}
+            {products.length < totalProducts && !isSearch ? (
+              <div
+                ref={loaderRef}
+                style={{ height: 20 }}
+              ><Loader size="sm"/></div>
             ) : null}
           </div>
         ) : (
